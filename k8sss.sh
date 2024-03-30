b05681df3d23b34fa1456188b7aaa7e6d1f4d2ef
Manish Kumar <95394412+Manish2408@users.noreply.github.com>
2024-03-30 23:24:30 +0530

    Update k8sss.sh

--- k8sss.sh
+++ k8sss.sh
@@ -8,11 +8,12 @@ apt install docker.io -y
 systemctl start docker
 systemctl enable docker
 
-curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
-echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 apt update -y
-apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+sudo apt-get install -y kubelet kubeadm kubectl
 
 # To connect with cluster execute above commands on master node and worker node respectively
 --------------------------------------------- Master Node -------------------------------------------------- 
